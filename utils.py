d2a5e290d7055192f9adf561430323c4a3c5b24f
sadjadtavakoli <sadjadtavakoli@gmail.com>
2022-07-15 03:04:18 -0700

    fix phone number' regex and message

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
 from django.core.validators import RegexValidator
 
-phone_number_regex = RegexValidator(regex=r'^(\+1)?[0-9]{10}$', message='Phone number should contain 10 digits.')
+phone_number_regex = RegexValidator(regex=r'^\d{3}[-]\d{3}[-]\d{4}$',
+                                    message='Phone number must be formatted as ###-###-####.')
