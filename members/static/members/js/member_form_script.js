affc38a636b21fd879ec3f6856f402889027cc96
sadjadtavakoli <sadjadtavakoli@gmail.com>
2022-07-16 20:44:46 -0700

    fix edit phone js bug

--- members/static/members/js/member_form_script.js
+++ members/static/members/js/member_form_script.js
@@ -18,16 +18,19 @@ $(document).ready(function () {
         });
     });
 
-    $('input[name="phone"]').on('input', function(){
-        let value = this.value.replaceAll('-','');
-        const areaCode = value.substring(0,3);
-        const middle = value.substring(3,6);
-        const last = value.substring(6,10);
-        let replacement = areaCode;
-        if(areaCode.length === 3) replacement+="-";
-        if(middle) replacement+=middle;
-        if(middle.length === 3) replacement+="-";
-        if(last) replacement+=last;
-        this.value= replacement;
+    $('input[name="phone"]').on('keyup', function (event) {
+        let key = event.keyCode || event.charCode;
+        if (key !== 8 && key !== 46) {
+            let value = this.value.replaceAll('-', '');
+            const areaCode = value.substring(0, 3);
+            const middle = value.substring(3, 6);
+            const last = value.substring(6, 10);
+            let replacement = areaCode;
+            if (areaCode.length === 3) replacement += "-";
+            if (middle) replacement += middle;
+            if (middle.length === 3) replacement += "-";
+            if (last) replacement += last;
+            this.value = replacement;
+        }
     });
 });
